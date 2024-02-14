8c2b1648ca92a2f0bccf8f79360dfbe4dc990993
Diego Pahua Silvan <dpahuas@gmail.com>
2024-02-14 15:10:33 -0600

    fix features

---
+++ src/components/backgrounds.js
@@ -0,0 +1,12 @@
+export const backgrounds = [
+    {
+        "background_tailwind":`<div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>`,
+        "background_css": ""
+    },
+    {
+        background_tailwind:`<div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950" style="background-image:linear-gradient(38.73deg, rgba(204, 0, 187, 0.25) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.25) 100%)"></div>`
+    },
+    {
+        background_tailwind:`<div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>`
+    }
+]
