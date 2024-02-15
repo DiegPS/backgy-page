d846a6e690715273a4148cba73ef4ccc800c2037
Diego Pahua Silvan <dpahuas@gmail.com>
2024-02-14 20:57:39 -0600

    add backgrounds using ibelick

--- src/components/backgrounds.js
+++ src/components/backgrounds.js
@@ -14,5 +14,121 @@ export const backgrounds = [
     {
         background_tailwind: `<div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>`,
         background_css: ""
+    },
+    {
+        background_tailwind: `<div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div>`,
+        background_css: ""
+    },
+    {
+        background_tailwind: `<div class="absolute top-0 -z-10 h-full w-full bg-white"><div class="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute h-full w-full bg-white "><div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute h-full w-full bg-neutral-900"><div class="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]"></div></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute h-full w-full bg-slate-950"><div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute h-full w-full bg-black"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div><div class="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: `<div class="absolute h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div></div>`,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
+    },
+    {
+        background_tailwind: ``,
+        background_css: ``
     }
 ]
